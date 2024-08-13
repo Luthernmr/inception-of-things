e128779bb172fe0973c80838c43f6b5816040fa9
vboxuser <vboxuser@iot.myguest.virtualbox.org>
2024-08-13 16:21:57 +0200

    finish p2.v1

--- p2/scripts/provision_master.sh
+++ p2/scripts/provision_master.sh
@@ -1,4 +1,9 @@
 curl -sfL https://get.k3s.io |  INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --node-ip 192.168.56.110  --bind-address=192.168.56.110 --advertise-address=192.168.56.110" sh -
+
 echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
-mkdir -p /vagrant/token
-cp /var/lib/rancher/k3s/server/node-token /vagrant/token
+
+kubectl create namespace app1
+kubectl create namespace app2
+kubectl create namespace app3
+
+kubectl apply -f /vagrant/apps
--- p2/scripts/provision_worker.sh
+++
@@ -1,3 +0,0 @@
-export TOKEN_FILE="/vagrant/token/node-token"
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token-file $TOKEN_FILE  --node-ip=192.168.56.111" sh -
-echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
